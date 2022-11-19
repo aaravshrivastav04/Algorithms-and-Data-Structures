062e4dce8db6580b01a621af684ecc3bcc72ce2c
Aarav Shrivastav <aaravshrivastav04@outlook.com>
2022-11-19 22:11:29 +0530

    Uploaded sum_of_first_n_numbers.py
    In this program, we have used a recursive logic to find out the sum of the first n numbers

---
+++ Recursion/sum_of_first_n_numbers.py
@@ -0,0 +1,15 @@
+def sumof(n):  # Our function will take a number n as input
+    assert type(n) in [int, float], "n should be a number"  # n should be a number only
+    if n in [1, 0]:  # If n is 0 or 1, we return the same value to the user
+        return n
+    else:
+        # The sum of the first n numbers can be written as
+        # n + (n-1) + (n-1) .... 0
+        # This can be further written as :
+        # n + sum of first (n-1) numbers
+        # Here's where the recursion happens
+        # We have simply coded this logic out in our program
+        return n + sumof(n - 1)
+
+
+print(sumof(4))  # We finally, test our function by giving it sample value of 4
